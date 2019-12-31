27831192988f86cfd81169d4681365432a512f5b
Esther Bernal <estherbernal@users-MacBook-Pro.local>
2019-12-31 20:21:17 +0100

    cors

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
-const createError = require('http-errors');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const createError = require("http-errors");
 
-const authRouter = require('./routes/auth');
-const contactsRouter = require('./routes/contacts');
+const authRouter = require("./routes/auth");
+const contactsRouter = require("./routes/contacts");
 
 const app = express();
 
 app.use(
   cors({
     credentials: true,
-    origin: 'http://localhost:3000'
+    origin: "http://localhost:3000" //mirar como poner para todo localhost
   })
 );
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/auth', authRouter);
-app.use('/contacts', contactsRouter);
+app.use("/auth", authRouter);
+app.use("/contacts", contactsRouter);
 
 app.use((error, req, res, next) => {
   res.status(error.status).send(error.message);
